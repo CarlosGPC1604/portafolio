48a1af32bfee0b7b5d3e06edd494b01b77bac930
carlo <carlosgahelpulidocervantes@gmail.com>
2023-10-20 22:04:10 -0700

    Importadas skills con su respectivo icono de React Icons

--- components/SkillCard.tsx
+++ components/SkillCard.tsx
@@ -1,24 +1,56 @@
 import React from "react";
-import {Card, CardFooter, Image, Button} from "@nextui-org/react";
+import { Card } from "@nextui-org/react";
+import { FaJava, FaReact, FaFigma } from 'react-icons/fa';
+import { SiMicrosoftsqlserver, SiNestjs, SiCsharp, SiCplusplusbuilder, SiAdobeillustrator, SiAdobephotoshop } from 'react-icons/si';
+import { DiPostgresql } from 'react-icons/di';
+import { RiJavascriptFill, RiBootstrapFill } from 'react-icons/ri';
+import { BiLogoTypescript, BiLogoHtml5, BiLogoTailwindCss, BiLogoMongodb } from 'react-icons/bi';
 
-// Enumeración de habilidades con niveles asociados
 export enum SkillLevel {
     JavaScript = 3,
     TypeScript = 4,
     React = 5,
-    // ... otros lenguajes/tecnologías que desees agregar
+    Java = 3,
+    CSharp = 3,
+    CPlusPlus = 2,
+    SQLServer = 3,
+    PostgreSQL = 4,
+    NestJS = 4,
+    HTML = 4,
+    Bootstrap = 4,
+    TailwindCSS = 4,
+    Figma = 4,
+    AdobeIllustrator = 3,
+    AdobePhotoshop = 3,
+    MongoDB = 3,
 }
 
-// Componente SkillCard
+const technologyIcons: Record<keyof typeof SkillLevel, JSX.Element> = {
+    React: <FaReact />,
+    JavaScript: <RiJavascriptFill />,
+    TypeScript: <BiLogoTypescript />,
+    Java: <FaJava />,
+    CSharp: <SiCsharp />,
+    CPlusPlus: <SiCplusplusbuilder />,
+    SQLServer: <SiMicrosoftsqlserver />,
+    PostgreSQL: <DiPostgresql />,
+    NestJS: <SiNestjs />,
+    HTML: <BiLogoHtml5 />,
+    Bootstrap: <RiBootstrapFill />,
+    TailwindCSS: <BiLogoTailwindCss />,
+    Figma: <FaFigma />,
+    AdobeIllustrator: <SiAdobeillustrator />,
+    AdobePhotoshop: <SiAdobephotoshop />,
+    MongoDB: <BiLogoMongodb />,
+};
+
 export const SkillCard = ({ skill, level }: { skill: keyof typeof SkillLevel; level: number }) => {
-    // Aquí puedes determinar el tamaño de la tarjeta basado en el nivel
-    const cardSize = level * 50; // solo un ejemplo, puedes ajustar como desees
+    const cardSize = level * 50; // solo un ejemplo
 
     return (
         <Card style={{ width: cardSize, height: cardSize }}>
-            {/* Aquí puedes renderizar el ícono basado en el skill */}
-            <img src={`/icons/${skill}.png`} alt={`${skill} icon`} />
-            {/* ... cualquier otro contenido que desees */}
+            {technologyIcons[skill]}
+            {/* ... el resto del contenido ... */}
         </Card>
     );
 };
