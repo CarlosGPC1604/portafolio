f6fc1fc4766501232fa19622a0f6359f556c1cfc
carlo <carlosgahelpulidocervantes@gmail.com>
2023-10-20 22:31:12 -0700

    Estilizadas las SkillCards y el navbar

--- components/SkillCard.tsx
+++ components/SkillCard.tsx
@@ -26,29 +26,35 @@ export enum SkillLevel {
 }
 
 const technologyIcons: Record<keyof typeof SkillLevel, JSX.Element> = {
-    React: <FaReact />,
-    JavaScript: <RiJavascriptFill />,
-    TypeScript: <BiLogoTypescript />,
-    Java: <FaJava />,
-    CSharp: <SiCsharp />,
-    CPlusPlus: <SiCplusplusbuilder />,
-    SQLServer: <SiMicrosoftsqlserver />,
-    PostgreSQL: <DiPostgresql />,
-    NestJS: <SiNestjs />,
-    HTML: <BiLogoHtml5 />,
-    Bootstrap: <RiBootstrapFill />,
-    TailwindCSS: <BiLogoTailwindCss />,
-    Figma: <FaFigma />,
-    AdobeIllustrator: <SiAdobeillustrator />,
-    AdobePhotoshop: <SiAdobephotoshop />,
-    MongoDB: <BiLogoMongodb />,
+    React: <FaReact size="70%"/>,
+    JavaScript: <RiJavascriptFill size="70%"/>,
+    TypeScript: <BiLogoTypescript size="70%"/>,
+    Java: <FaJava size="70%"/>,
+    CSharp: <SiCsharp size="70%"/>,
+    CPlusPlus: <SiCplusplusbuilder size="70%"/>,
+    SQLServer: <SiMicrosoftsqlserver size="70%"/>,
+    PostgreSQL: <DiPostgresql size="70%"/>,
+    NestJS: <SiNestjs size="70%"/>,
+    HTML: <BiLogoHtml5 size="70%"/>,
+    Bootstrap: <RiBootstrapFill size="70%"/>,
+    TailwindCSS: <BiLogoTailwindCss size="70%"/>,
+    Figma: <FaFigma size="70%"/>,
+    AdobeIllustrator: <SiAdobeillustrator size="70%"/>,
+    AdobePhotoshop: <SiAdobephotoshop size="70%"/>,
+    MongoDB: <BiLogoMongodb size="70%"/>,
 };
 
 export const SkillCard = ({ skill, level }: { skill: keyof typeof SkillLevel; level: number }) => {
     const cardSize = level * 50; // solo un ejemplo
 
     return (
-        <Card style={{ width: cardSize, height: cardSize }}>
+        <Card style={{
+            width: cardSize, 
+            height: cardSize,
+            display: 'flex',   
+            alignItems: 'center', 
+            justifyContent: 'center' 
+        }}>
             {technologyIcons[skill]}
             {/* ... el resto del contenido ... */}
         </Card>
