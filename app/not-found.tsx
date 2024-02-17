7c90693d850e1af658460ea2c3bb1deaf0a690d1
Carlos Gahel Pulido Cervantes <carlosgahelpulidocervantes@gmail.com>
2024-02-16 17:38:51 -0700

    Actua

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,10 +1,13 @@
-import Link from 'next/link'
+import NextLink from "next/link";
+import { Link } from "@nextui-org/link";
+import { button as buttonStyles } from "@nextui-org/theme";
 import {Image} from "@nextui-org/image";
+import { title } from "@/components/primitives";
 
 export default function NotFound() {
     return (
-        <div>
-            <h2>Not Found</h2>
+        <div className='flex flex-col items-center justify-center gap-4 py-8 md:py-10'>
+            <h2 className={title()}>No hay nada por aquí</h2>
             <Image
                 isBlurred
                 disableSkeleton={false}
@@ -13,9 +16,17 @@ export default function NotFound() {
                 loading="lazy"
                 width={531}
                 height={1200}
-                className="m-5 hover:scale-105 transition duration-500 cursor-pointer"
+                className="my-10 hover:scale-105 transition duration-1000 ease-in-out cursor-pointer"
             />
-            <Link href="/">Return Home</Link>
+            <Link
+						isExternal
+						as={NextLink}
+						href= '/'
+                        target="_self"
+						className={buttonStyles({ color: "danger", radius: "full", variant: "shadow" })}
+					>
+						Regresar al inicio
+					</Link>
         </div>
     )
 }
