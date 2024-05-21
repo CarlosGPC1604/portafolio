1cc2dc81b836b06fdeaacb8b4ed2da60de179afa
Carlos Gahel Pulido Cervantes <carlosgahelpulidocervantes@gmail.com>
2024-05-20 20:56:52 -0700

    Corregida la resposividad del navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -20,10 +20,7 @@ import clsx from "clsx";
 
 import { ThemeSwitch } from "@/components/theme-switch";
 import {
-	TwitterIcon,
 	GithubIcon,
-	DiscordIcon,
-	HeartFilledIcon,
 	SearchIcon,
 	LinkedinIcon,
 } from "@/components/icons";
@@ -44,7 +41,7 @@ export const Navbar = () => {
 				</Kbd>
 			}
 			labelPlacement="outside"
-			placeholder="Search..."
+			placeholder="Buscar..."
 			startContent={
 				<SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
 			}
@@ -60,7 +57,7 @@ export const Navbar = () => {
 						<p className="font-bold text-inherit">Portafolio</p>
 					</NextLink>
 				</NavbarBrand>
-				<ul className="hidden lg:flex gap-4 justify-start ml-2">
+				<ul className="hidden sm:flex gap-4 justify-start ml-2">
 					{siteConfig.navItems.map((item) => (
 						<NavbarItem key={item.href}>
 							<NextLink
@@ -94,6 +91,9 @@ export const Navbar = () => {
 			</NavbarContent>
 
 			<NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
+				<Link isExternal href={siteConfig.links.linkedin} aria-label="linkedin">
+						<LinkedinIcon className="text-default-500" />
+				</Link>
 				<Link isExternal href={siteConfig.links.github} aria-label="Github">
 					<GithubIcon className="text-default-500" />
 				</Link>
@@ -108,13 +108,10 @@ export const Navbar = () => {
 						<NavbarMenuItem key={`${item}-${index}`}>
 							<Link
 								color={
-									index === 2
-										? "primary"
-										: index === siteConfig.navMenuItems.length - 1
-										? "danger"
-										: "foreground"
+									index === 3
+										? "danger" : "foreground"
 								}
-								href="#"
+								href={item.href}
 								size="lg"
 							>
 								{item.label}
